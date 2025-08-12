3ce5e39d99bd29039a56f25e6d3725f886f48060
lmmere <lmmeredith77@gmail.com>
2025-08-12 17:23:11 -0500

    Add is_prime function for Prime Time challenge

---
+++ is_prime.py
@@ -0,0 +1,10 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(11))  # True
+print(is_prime(4))   # False    
